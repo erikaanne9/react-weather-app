44ce749a9f417069c23d24f01c50ba09e2f91769
erikaanne9 <erikaanne9@gmail.com>
2022-07-24 14:13:40 -0500

    Added footer with github link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Erika Jones and is
+        <a href="https://github.com/erikaanne9/react-weather-app">
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
