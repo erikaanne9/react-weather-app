f53e8a70a7c9d3970902fcc3b515b38466a9494d
erikaanne9 <erikaanne9@gmail.com>
2022-07-24 16:31:09 -0500

    Added styling to weather component

--- src/Weather.js
+++ src/Weather.js
@@ -8,17 +8,30 @@ export default function Weather() {
       <form>
         <div className="row">
           <div className="col-9">
-            <input type="search" placeholder="Type a city.." />
+            <input
+              type="search"
+              placeholder="Type a city.."
+              autoFocus="on"
+              className="formControl"
+            />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
       <h1>New York Weather Conditions</h1>
-      <h3>Thursday 10:00 am</h3>
-      <h3>80°F</h3>
-      <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="Clear" />
+      <h2>Thursday 10:00 am</h2>
+      <div className="currentTemp">
+        <img src="https://openweathermap.org/img/wn/02d@2x.png" alt="Clear" />
+        <span className="temperature">80</span>
+        <span className="unit">°F</span>
+      </div>
+
       <ul>
         <li>Sunny</li>
         <li>Humidity 50%</li>
